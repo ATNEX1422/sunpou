e52401e2a7d6ad95843c208f847756f8721af633
ATNEX1422 <kntkhr.1422@icloud.com>
2026-05-17 16:56:08 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -306,7 +306,7 @@ const createDimensionAtPosition = (
       }
 
 const combinedText = new fabric.IText(combinedTextString, {
-        fontSize: 10,
+        fontSize: 1,
         fontWeight: 'bold',
         fill: color,
         backgroundColor: 'rgba(255, 255, 255, 0.85)',
